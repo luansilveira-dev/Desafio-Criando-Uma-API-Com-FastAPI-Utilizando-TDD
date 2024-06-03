09a3b69802a7e57cc18c13115fde7f1b19343ae6
Luan <luanbsilveira23@gmail.com>
2024-06-03 14:27:47 -0300

    apitdd: update

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 import os 
 
-
-os.system("uvicorn store.main:app --reload")
+def run_api():
+  return os.system("uvicorn store.main:app --reload")
 
 #poetry run pre-commit install
+def test():
+  return os.system("poetry run pytest")
+
+
+if __name__ == "__main__":
+  test()
---
+++ store/schemas/base.py
@@ -0,0 +1,9 @@
+from datetime import datetime, timezone
+from uuid import uuid4
+from pydantic import UUID4, BaseModel, Field
+
+
+class BaseSchemaMixin(BaseModel):
+    id: UUID4 = Field(default_factory=uuid4)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
